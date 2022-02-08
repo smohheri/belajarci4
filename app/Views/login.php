0509597c3314b9598b7b877cd7aa16190cd330bc
smohheri <smohheri@gmail.com>
2022-02-08 11:35:49 +0800

    form login

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,15 +6,50 @@
         <div class="col-6">
             <div class="card card-blue">
                 <div class="card-header ">
-                    Login siswa
+                    <form action="" method="post">
+                        <div class="card-title" style="text-align: center;">
+                            Login Siswa
+                        </div>
                 </div>
                 <div class="card-body">
-        Badan            
+                    <div class="form-group">
+                        <label for="">NIS : </label>
+                        <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password : </label>
+                        <input type="password" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                    </div>
                 </div>
-<div class="card-footer">
-    Kaki
-</div>
+                <div class="card-footer">
+                    <span class="btn btn-primary float-right">Login</span>
+                </div>
+                </form>
             </div>
+        </div>
+        <div class="col-6">
+            <form action="" method="post">
+                <div class="card card-danger">
+                    <div class="card-header">
+                        <div class="card-title" style="text-align: center;">
+                            Login Guru
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="">NIY : </label>
+                            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Password :</label>
+                            <input type="password" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <span class="btn btn-danger float-right">Login</span>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
     <!-- end content wrapper -->
